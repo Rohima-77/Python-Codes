7e5be1c78888c38d2fc1169512abf7a051c6b6f5
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-07-09 16:34:30 +0600

    Create List&ListMethods.py

---
+++ List&ListMethods.py
@@ -0,0 +1,35 @@
+a = [1,7,3,'Turi','Jessi','Bithi',6]
+print(a)
+print(a[-1])
+print(len(a))
+
+#split korte chaile:
+s =("Hello")
+print(list(s))
+
+#List Methods:
+#append():	Adds an element at the end of the list. means:list er last er dike add hbe
+a.append([3,5,8])
+print(a)
+
+#clear(): Removes all the elements from the list
+#a.clear()
+#print(a)
+
+#copy(): Returns a copy of the list
+a.copy()
+print(a)
+
+#count(): Returns the number of elements with the specified value
+#a.count()
+#print(a)
+
+#extend(): Add the elements of a list (or any iterable), to the end of the current list
+a.extend([5])
+print(a)
+
+#reverse() :Reverses the order of the list
+a.reverse()
+print(a)
+
+# index(),insert(),pop(),remove(),sort()
