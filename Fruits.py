75f06116661e28a7d3cb791bb424bd7b7fd967bc
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-07-13 23:16:24 +0600

    Create Fruits.py

---
+++ Fruits.py
@@ -0,0 +1,11 @@
+Write a function that sorts a list of strings alphabetically. 
+
+["apple", "banana", "cherry", "kiwi", "grape"]
+
+Solve:
+def sort_strings_alphabetically(string_list):
+    return sorted(string_list)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "grape"]
+sorted_fruits = sort_strings_alphabetically(fruits)
+print(sorted_fruits)
