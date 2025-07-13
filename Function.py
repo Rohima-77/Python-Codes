23f6b1c56673db269054da34b30743eba24cb0ae
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-07-13 23:08:21 +0600

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,39 @@
+# There are two types of function:
+        # 1.User-defined function.#create korte hoy
+        # 2.Built in function.#Create korai thake
+        
+        
+        
+# User defined function:
+
+#Type:01:No input, no return
+def my_first_function(): #function define korlam
+    a = 10
+    b = 13
+    print(a+b)
+my_first_function() #function call korlam
+
+
+#Type:02:Input , no return
+def add_two_numbers(a,b):# argument
+    print(a+b)
+add_two_numbers(12,14) #parameters
+
+
+#Type:03:input,return
+def multiply_two_nums(a,b):
+    return a*b
+result = multiply_two_nums(12,9)
+print(result)
+
+#Type:04:no input,return
+def hello():
+    return "Hello"
+result = hello()
+print (result)
+
+
+
+
+
+
