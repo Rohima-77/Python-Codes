b592ef43ebd7b595e129a64bd9ffaa6aa8ba29ef
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-07-13 23:14:12 +0600

    Create CalculateAverage.py

---
+++ CalculateAverage.py
@@ -0,0 +1,10 @@
+Write a function that takes two numbers as input and returns their average. Call the function with different values.
+
+Solve:
+def calculate_average(num1, num2):
+    return (num1 + num2) / 2
+
+print(calculate_average(9, 6))    
+print(calculate_average(-9, 9))   
+print(calculate_average(0, 0))   
+print(calculate_average(8.5, 5.5)) 
