a6beb7791fbf8b746bbc0abba641412fdb9e5653
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-07-24 12:36:47 +0600

    Create Matrix Transpose.py

---
+++ Matrix Transpose.py
@@ -0,0 +1,10 @@
+Problem: Ekta matrix er transpose (row <-> column) koro.
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+transpose = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+print(transpose)
